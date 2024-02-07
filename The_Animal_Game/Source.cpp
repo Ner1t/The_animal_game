d5383634761ee6ffeb8889cc29d8d8c61931fbcb
Roman Goriachev <roman.nerit@gmail.com>
2024-02-07 21:38:03 +0300

    improving functionality, adding comments

--- The_Animal_Game/Source.cpp
+++ The_Animal_Game/Source.cpp
@@ -5,68 +5,82 @@ using namespace std;
 
 int main()
 {
-	/*system( "chcp 1251" );
+	/*system( "chcp 1251" );     //Оставил на всякий случай.
 	setlocale( LC_ALL, "" );*/
 
 	map<int, TAnimal> data;
 	bool flagEndCycle = false;
 	bool guessedRight = false;
-	string yes = "да";
-	string no = "нет";
 	vector<string> exclusionAnimals;
 	vector<string> exclusionNamesOfAnimals;
+	string yes = "да";
+	string no = "нет";
 	string answer;
+	int inner_key;
 	largest_key = 1;
 
-	loadFromFile(data);
+	loadFromFile( data );
 
-	if (data.empty())
+	if ( data.empty() )
 	{
 		data[0] = { "гончая собака", "собака" };
-		saveToFile(data);
-		loadFromFile(data);
+		saveToFile( data );
+		loadFromFile( data );
 	}
 
-	auto firstElement = prev(data.end());
+	auto firstElement = prev( data.end() );
 	largest_key = firstElement->first;
-	int inner_key = largest_key;
+	inner_key = largest_key;
+
 	cout << "Задумайте животное.\n";
+	cout << "\n";
 
-	while (!flagEndCycle)
+	while ( !flagEndCycle )
 	{
-		for (inner_key; inner_key > 0; inner_key--)
+		for ( inner_key; inner_key >= 0; inner_key-- )
 		{
+			auto it = find( exclusionAnimals.begin(), exclusionAnimals.end(), data[inner_key].question ); // проверка на уникальность типа животного.
 
-			auto it = find(exclusionAnimals.begin(), exclusionAnimals.end(), data[inner_key].question);
-
-			if ((it == exclusionAnimals.end()) || exclusionAnimals.empty())
+			if ( ( it == exclusionAnimals.end() ) || exclusionAnimals.empty() )
 			{
-				cout << "Это животное - " + data[inner_key].question + "?\n";
+				if ( data.size() == 1 ) // стартовое условие.
+				{
+					cout << "Это животное - " + data[inner_key].name + "?\n";
+				}
+				else
+				{
+					cout << "Это животное - " + data[inner_key].question + "?\n";
+				}
+
 				cin >> answer;
 			}
 
-			addUnique(exclusionAnimals, data[inner_key].question);
+			addUnique( exclusionAnimals, data[inner_key].question );
 
-			if ((answer != no) && (answer != yes))
+			if ( ( answer != no ) && ( answer != yes ) )
 			{
 				cout << "Нужно ответить „да“ либо „нет“, попробуйте еще раз.\n";
 				flagEndCycle = true;
 				break;
 			}
 
-			if (answer == yes)
+			if ( answer == yes )
 			{
 				string typeOfAnimal = data[inner_key].question;
 
-				for (inner_key; inner_key > 0; inner_key--)
+				for ( inner_key; inner_key >= 0; inner_key-- )
 				{
-					if (data[inner_key].question == typeOfAnimal)
+					if ( data[inner_key].question == typeOfAnimal ) //условие для работы с подтипами.
 					{
-						addUnique(exclusionNamesOfAnimals, data[inner_key].name);
-						cout << data[inner_key].name + "?\n";
-						cin >> answer;
+						addUnique( exclusionNamesOfAnimals, data[inner_key].name );
 
-						if (answer == yes)
+						if ( data.size() != 1 ) //стартовое условие.
+						{
+							cout << data[inner_key].name + "?\n";
+							cin >> answer;
+						}
+
+						if ( answer == yes )
 						{
 							cout << "Ура, я угадала!\n";
 							flagEndCycle = true;
@@ -76,48 +90,50 @@ int main()
 					}
 				}
 
-				if (!guessedRight)
+				if ( !guessedRight ) // случай когда такого ПОДТИПА не существует в базе.
 				{
 					cout << "Увы, я не угадала ваше животное. Назовите мне его и введите вопрос, на который нужно ответить „да“ для вашего животного и „нет“ для ";
 
-					for (auto& value : exclusionNamesOfAnimals)
+					for ( auto& value : exclusionNamesOfAnimals )
 					{
 						cout << value << ' ';
 					}
 
 					cout << "\n";
 					cout << "Формат ввода текста:  имя_животного это вид_животного? \n";
-					createAndAddNewEntry(data);
+					auto nameType = createNewEntry();
+					addNewEntry( data, nameType.first, nameType.second );
 					flagEndCycle = true;
 					cout << "Спасибо, теперь я знаю что вы загадали!\n";
 					break;
 				}
 
-				if (flagEndCycle)
+				if ( flagEndCycle )
 				{
 					break;
 				}
 			}
 
-			if (data[inner_key].name == "гончая собака")
+			if ( data[inner_key].name == "гончая собака" ) //случай когда такого ТИПА не существует в базе.
 			{
 				cout << "Увы, я не угадала ваше животное. Назовите мне его и введите вопрос, на который нужно ответить „да“ для вашего животного и „нет“ для ";
 
-				for (auto& value : exclusionAnimals)
+				for ( auto& value : exclusionAnimals )
 				{
 					cout << value << ' ';
 				}
 
 				cout << "\n";
 				cout << "Формат ввода текста: „имя_животного это вид_животного?“\n";
-				createAndAddNewEntry(data);
+				auto nameType = createNewEntry();
+				addNewEntry( data, nameType.first, nameType.second );
 				flagEndCycle = true;
 				cout << "Спасибо, теперь я знаю что вы загадали!\n";
 				break;
 			}
 		}
 	}
-
-}
+	return 0;
+};
 
 
--- The_Animal_Game/TProcessor.cpp
+++ The_Animal_Game/TProcessor.cpp
@@ -5,10 +5,10 @@
 #include <vector>
 using namespace std;
 
-
 namespace TheAnimalGame
 {
-	static  int largest_key;
+
+	static int largest_key; // используется для нумерации каждой новой записи в data.txt
 
 	struct TAnimal
 	{
@@ -16,86 +16,110 @@ namespace TheAnimalGame
 		string question;
 	};
 
-	static void saveToFile(map<int, TAnimal>& data)
+	//! Основной оператор
+	//! Принимает ссылку на map с входными данными data
+	//! Сохраняет новые данные путем Пересоздания файла data.txt
+	//!  с актуальными параметрами которые беруться из входного параметра.
+	static void saveToFile( map<int, TAnimal>& data )
 	{
-		ifstream existingFile("data.txt");
-		ofstream newFile("data_temp.txt");
+		ifstream existingFile( "data.txt" );
+		ofstream newFile( "data_temp.txt" );
 
-		for (auto& pair : data)
+		for ( auto& pair : data )
 		{
 			newFile << pair.first << "\n";
 			newFile << pair.second.name << "\n";
 			newFile << pair.second.question << "\n";
 		}
 
-		if (existingFile.is_open())
+		if ( existingFile.is_open() )
 		{
 			newFile << existingFile.rdbuf();
 			existingFile.close();
 		}
 
 		newFile.close();
-		remove("data.txt");
-		ignore = rename("data_temp.txt", "data.txt");
-	}
+		remove( "data.txt" );
+		ignore = rename( "data_temp.txt", "data.txt" );
+	};
 
-	static string removePrefixAndSuffix(string question)
+	//! Основной оператор
+	//! Принимает строку question и редактирует ее по шаблонам. 
+	//! Цель форматирования получить информацию по которой будет происходить сортировка.
+	//! Возвращает отредактированную строку question.
+	static string removePrefixAndSuffix( string question )
 	{
 		std::string prefix = "это ";
 		std::string suffix = "?";
 
-		if (question.rfind(prefix, 0) == 0)
+		if ( question.rfind( prefix, 0 ) == 0 )
 		{
-			question.erase(0, prefix.length());
+			question.erase( 0, prefix.length() );
 		}
 
-		if (!question.empty() && question[question.length() - 1] == suffix[0])
+		if ( !question.empty() && question[question.length() - 1] == suffix[0] )
 		{
-			question.erase(question.length() - 1);
+			question.erase( question.length() - 1 );
 		}
 
 		return question;
-	}
+	};
 
-	static void loadFromFile(map<int, TAnimal>& data)
+	//! Основной оператор
+	//! Принимает ссылку на map с входными данными data. 
+	//! Загружает информацию из файла data.txt и заполняет ею data экземпляр структуры map.
+	static void loadFromFile( map<int, TAnimal>& data )
 	{
-		ifstream file("data.txt");
-		if (file.is_open())
+		ifstream file( "data.txt" );
+		if ( file.is_open() )
 		{
 			string key;
-			while (getline(file, key))
+			while ( getline( file, key ) )
 			{
 				TAnimal animal;
-				getline(file, animal.name);
-				getline(file, animal.question);
-
-				int int_key = stoi(key);
-				++int_key;
-
+				getline( file, animal.name );
+				getline( file, animal.question );
+				int int_key = stoi( key );
 				data[int_key] = animal;
 			}
 			file.close();
 		}
-	}
+	};
 
-	static void createAndAddNewEntry(map<int, TAnimal>& data)
+	//! Основной оператор
+	//! Получает из консоли 2 строки, обрабатывает и возращает. 
+	//! Возвращаемое значение это пара строк.
+	static pair<string, string> createNewEntry()
 	{
 		string newAnimal, newQuestion;
 		cin >> newAnimal;
 		cin.ignore();
-		getline(cin, newQuestion);
-		newQuestion = removePrefixAndSuffix(newQuestion);
-		data.clear();
-		data[largest_key] = { newAnimal, newQuestion };
-		saveToFile(data);
-		loadFromFile(data);
-	}
+		getline( cin, newQuestion );
+		newQuestion = removePrefixAndSuffix( newQuestion );
+		return make_pair( newAnimal, newQuestion );
+	};
 
-	static void addUnique(vector<string>& vec, string value)
+	//! Основной оператор
+	//! Принимает ссылку на map с входными данными data, и две строки.
+	//! Добавляет в data новую запись с добавлением индекса.
+	static void addNewEntry( map<int, TAnimal>& data, string newAnimal, string newQuestion )
 	{
-		if (find(vec.begin(), vec.end(), value) == vec.end())
+		data.clear();
+		data[++largest_key] = { newAnimal, newQuestion };
+		saveToFile( data );
+		loadFromFile( data );
+	};
+
+	//! Основной оператор
+	//! Принимает ссылку на vector с входными данными vec, строка value.
+	//! Проверяет value и добавляет в vec только если она уникальна для vec.
+	static void addUnique( vector<string>& vec, string value )
+	{
+		if ( find( vec.begin(), vec.end(), value ) == vec.end() )
 		{
-			vec.push_back(value);
+			vec.push_back( value );
 		}
-	}
-}
+	};
+
+};
+
